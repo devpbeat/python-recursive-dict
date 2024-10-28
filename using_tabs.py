86dfd33e60bb97e1539f77de82ba4aed2853a8d0
Nahuel Pereira <73185498+devpbeat@users.noreply.github.com>
2024-10-28 20:18:55 +0000

    added support for using tabs instead of blank space * ident

---
+++ using_tabs.py
@@ -0,0 +1,42 @@
+"""
+Dado el siguiente diccionario, 
+realiza una función recursiva para imprimir la estructura ordenada 
+por indentación (cada indentación es de 4 espacios)
+"""
+complex_dict = {
+    'a': 1,
+    'b': {
+        'c': 2,
+        'd': {
+            'e': {
+                'f': {
+                    'g': 3,
+                    'h': 4
+                },
+                'i': 5
+            },
+            'j': 6
+        }
+    },
+    'k': {
+        'l': {
+            'm': 7,
+            'n': {
+                'o': 8,
+                'p': {
+                    'q': 9
+                }
+            }
+        }
+    }
+}
+
+def print_ident_dictionary(dictionary, indent=0):
+    for key, value in dictionary.items():
+        print("\t" * indent + str(key) + ":")
+        if isinstance(value, dict):
+            print_ident_dictionary(value, indent + 1)
+        else:
+            print("\t" * (indent + 1) + str(value))
+
+print_ident_dictionary(complex_dict)
